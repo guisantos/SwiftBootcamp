d47fd643f7ec48f82690a56b35a6bab2ef6bbf13
Guilherme Santos <guilhermefloresdev@gmail.com>
2020-06-10 14:18:02 +0100

    added Quizzler project

---
+++ Quizzler-iOS13/Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Guilherme Flores on 09/06/2020.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answers: [String]
+    let correctAnswer: String
+    
+    init(q: String, a: [String], correctAnswer: String) {
+        text = q
+        answers = a
+        self.correctAnswer = correctAnswer
+    }
+}
